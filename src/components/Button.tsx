0903be0e7165af499a44214d1ef8f6e0a8e059a7
astracas2fs <161399339+astracas2fs@users.noreply.github.com>
2025-04-20 20:22:03 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,19 +5,22 @@ type ButtonProps = {
   className?: string;
   onClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
+  disabled?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({ 
   children, 
   className = '', 
   onClick,
-  type = 'button'
+  type = 'button',
+  disabled = false
 }) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`bg-black text-white text-sm rounded-full px-4 py-2 font-medium transition-all duration-300 hover:bg-opacity-80 ${className}`}
+      disabled={disabled}
+      className={`bg-black text-white text-sm rounded-full px-4 py-2 font-medium transition-all duration-300 hover:bg-opacity-80 ${disabled ? 'opacity-50 cursor-not-allowed' : ''} ${className}`}
     >
       {children}
     </button>
